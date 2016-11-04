32d07b1efee090b1088e116ae217d99e2ad653c6
Raja Simon <rajasimon@icloud.com>
2016-11-04 23:22:35 +0530

    two postgresql database support settings

--- villagescc/settings.py
+++ villagescc/settings.py
@@ -76,8 +76,20 @@ WSGI_APPLICATION = 'villagescc.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cc_db_one',
+        'USER': 'simon',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+    },
+    'second_db': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cc_db_two',
+        'USER': 'simon',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
