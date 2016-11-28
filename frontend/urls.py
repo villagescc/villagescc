abaac688d58a8d9c962f5a554a00aa12a66b1c88
Raja Simon <rajasimon@icloud.com>
2016-11-28 09:08:54 +0530

    switching navigation and jumbotron based on user authentication

--- frontend/urls.py
+++ frontend/urls.py
@@ -5,6 +5,5 @@ from frontend import views
 
 urlpatterns = [
     url(r'^$', views.home),
-    url(r'^post-login$', views.post_login_home),
     url(r'^profile$', views.profile),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -8,15 +8,7 @@ def home(request):
 	returns before login home.
 	url: /home
 	"""
-	return render(request, 'pages/pre-login-home.html')
-
-
-def post_login_home(request):
-	"""
-	url: /after-login
-	return: post-login-home.html
-	"""
-	return render(request, 'pages/post-login-home.html')
+	return render(request, 'pages/home.html')
 
 
 def profile(request):
