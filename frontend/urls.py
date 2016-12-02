83ee97573ab7aa7a41ef704b6b00eaccb5c635b4
Raja Simon <rajasimon@icloud.com>
2016-12-03 00:14:47 +0530

    Element tag fixed in html and styling update and post-login page

--- frontend/urls.py
+++ frontend/urls.py
@@ -5,7 +5,8 @@ from frontend import views
 
 urlpatterns = [
     url(r'^$', views.home),
-    url(r'^profile$', views.profile),
-    url(r'^payment$', views.payment),
-    url(r'^endorsement$', views.endorsement),
+    url(r'^post-login/$', views.post_login),
+    url(r'^profile/$', views.profile),
+    url(r'^payment/$', views.payment),
+    url(r'^endorsement/$', views.endorsement),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -12,6 +12,10 @@ def home(request):
 	return render(request, 'pages/home.html', {'listings': listings})
 
 
+def post_login(request):
+	listings = [1,2,3,5]
+	return render(request, 'pages/post-login.html', {'listings': listings})
+
 def profile(request):
 	listings = [1,2,3,4,5,5]
 	return render(request, 'pages/profile.html', {'listings': listings})
