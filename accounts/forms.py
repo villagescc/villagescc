00f5581052389c1026ba46bf7a98691f8c765bcb
Raja Simon <rajasimon@icloud.com>
2016-12-11 23:14:14 +0530

    Email required for the signup and comments

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,6 +3,13 @@ from django.contrib.auth.models import User
 
 # Forms
 class RegisterForm(ModelForm):
+    """ Signup form """
+
+    def __init__(self, *args, **kwargs):
+        # Overriding form to set email is required because User email is optional
+        super(RegisterForm, self).__init__(*args, **kwargs)
+        self.fields['email'].required = True
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
--- accounts/views.py
+++ accounts/views.py
@@ -1,53 +1,83 @@
-from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
+# Django http and shortcuts import
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
+# From Contrib
+from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+
+# Django User
 from django.contrib.auth.models import User
+
 # App Forms
 from accounts.forms import RegisterForm
 
+
 # Create your views here.
 def signup_view(request):
+    """
+    POST method with email and username and password register new user
+    """
     if request.method == 'POST':
-        register_form = RegisterForm(request.POST)
-        if register_form.is_valid():
-            register_form.save(commit=False)
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save(commit=False)
 
-            email = register_form.cleaned_data['email']
-            username = register_form.cleaned_data['username']
-            password = register_form.cleaned_data['password']
+            email = form.cleaned_data['email']
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
             user = User.objects.create_user(username, email, password)
 
+            # Successfull login redirect to login page
             return HttpResponseRedirect(reverse('accounts:login'))
         else:
-            print register_form.errors
-            return render(request, 'accounts/login.html', {'form': register_form})
+            # If error found we need to show to the frontend using form errors
+            # Returning same form contains context of errors attripute
+            return render(request, 'accounts/signup.html', {'form': form})
     else:
+        # Get method should return empty Form
         register_form = RegisterForm()
     return render(request, 'accounts/signup.html', {'form': register_form})
 
 
 def login_view(request):
+    """
+    Login view function takes no arguemnt if supplied username and password
+    matches then request will authenticate with login and return with User context
+    """
     if request.method == 'POST':
+
+        # Will not throw error because input have value=""
         username = request.POST['username']
         password = request.POST['password']
+
+        # Check the user in database or return
         try:
             user = User.objects.get(username=username)
             user = authenticate(username=username, password=password)
             if user:
+                # Password matching and user found with authenticate
                 login(request, user)
-            return HttpResponseRedirect(reverse('frontend:home'))
+                return HttpResponseRedirect(reverse('frontend:home'))
+            else:
+                # Password wrong
+                messages.error(request, 'Username or Password is wrong')
         except:
-            return render(request, 'accounts/login.html')
+            messages.error(request, "User not found")
+            return HttpResponseRedirect(reverse('accounts:login'))
     return render(request, 'accounts/login.html')
 
 
 def logout_view(request):
+    """ Remove user from the request """
     logout(request)
     return HttpResponseRedirect(reverse("frontend:home"))
 
 
 def profile(request):
-	listings = [1,2,3,4,5,5]
-	return render(request, 'accounts/profile.html', {'listings': listings})
+    """
+    Profile takes no arugment and returns the listings attripute
+    """
+    listings = [1,2,3,4,5,5]
+    return render(request, 'accounts/profile.html', {'listings': listings})
