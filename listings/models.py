6292b783ad102d55b9ad6e8df92486dd7c4d6cc7
Raja Simon <rajasimon@icloud.com>
2016-12-16 09:10:43 +0530

    Categories in html and supports

--- listings/models.py
+++ listings/models.py
@@ -3,11 +3,15 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
+# Categories Models
+from categories.models import SubCategories
+
 # Create your models here.
 class Listings(models.Model):
     user = models.ForeignKey(User, null=True, blank=True)
     title = models.CharField(max_length=220)
     price = models.FloatField()
+    subcategories = models.ForeignKey(SubCategories, null=True, blank=True)
 
     OFFER = 'Offer'
     REQUEST = 'Request'
